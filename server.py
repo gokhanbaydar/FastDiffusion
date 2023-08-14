24925c3d80f4eccbe626a1fda1447403c88b6613
GB <gokhanbaydar@yahoo.com>
2023-08-14 21:42:07 +0300

    init

---
+++ server.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
